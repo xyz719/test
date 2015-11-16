a00a429ac95c78ddca77f7d6eb7186d65fc4d545
xyz719 <953210274@qq.com>
2015-11-16 16:21:19 +0800

    1.0
    寻找花朵花瓣数

--- test/test.cpp
+++ test/test.cpp
@@ -15,14 +15,14 @@ int main()
 	      waitKey();
 
      return 0;
-}*/
+}
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 using namespace cv;
 using namespace std;
 
-/*using namespace cv;
+using namespace cv;
 using namespace std;
 void drawDetectLines(Mat& image,const vector<Vec4i>& lines,Scalar & color)
 {
@@ -35,7 +35,7 @@ void drawDetectLines(Mat& image,const vector<Vec4i>& lines,Scalar & color)
         line(image,pt1,pt2,color,2); //  线条宽度设置为2
         ++it;
     }
-}*/
+}
 int main()
 {
      Mat img = imread("f:\\beauty.jpg");
@@ -58,4 +58,35 @@ int main()
 	 imshow("123",img);
      waitKey();
      return 0;
+}*/
+//#include "stdafx.h"
+#include <opencv2/opencv.hpp>  
+#include <opencv\cv.h>
+#include <opencv\highgui.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>  
+//using namespace std;  
+using namespace cv; 
+
+int main( int argc, char** argv )  
+{  
+    Mat src, edge,color_edge;  
+    src=imread("f:\\beauty.jpg");  
+    if( !src.data )    
+        return -1;    
+  
+    Canny(src,edge,50,200,3);  
+    cvtColor( edge, color_edge, CV_GRAY2BGR );  
+    vector<Vec4i> lines;  
+    HoughLinesP(edge, lines, 1, CV_PI/180, 80, 30, 10 );  
+    for( size_t i = 0; i < lines.size(); i++ )  
+    {  
+        Vec4i l = lines[i];  
+        line( color_edge, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 2);  
+    }    
+    namedWindow( "lines", CV_WINDOW_AUTOSIZE );  
+    imshow( "lines", color_edge );  
+    waitKey(0);  
+    return 0;  
 }
